e25e17fe12da740bebb1ef08ccc7052a669ff1a2
GOVIND RAM <129197467+GOVIND-RAM@users.noreply.github.com>
2024-12-22 18:05:55 +0530

    Add files via upload

---
+++ API/index.js
@@ -0,0 +1,18 @@
+export const getOrders = () => {
+  return fetch("https://dummyjson.com/c/9263-15e5-4549-939c").then((res) => res.json());
+};
+
+export const getRevenue = () => {
+  return fetch("https://dummyjson.com/carts").then((res) => res.json());
+};
+
+export const getInventory = () => {
+  return fetch("https://dummyjson.com/products").then((res) => res.json());
+};
+
+export const getCustomers = () => {
+  return fetch("https://dummyjson.com/users").then((res) => res.json());
+};
+export const getComments = () => {
+  return fetch("https://dummyjson.com/comments").then((res) => res.json());
+};
